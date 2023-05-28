dae335501e666d12f88fcf9d343daddef344571f
Surya M <surya.m@phonepe.com>
2023-05-28 13:26:17 +0530

    updated user response

--- internal/controllers/user_controller.go
+++ internal/controllers/user_controller.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
-	"math"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/madeinatria/love-all-backend/internal/models"
+	"github.com/madeinatria/love-all-backend/internal/utils"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -22,27 +22,32 @@ func NewUserController(db *gorm.DB) *UserController {
 func (uc *UserController) GetAllUsers(c *gin.Context) {
 	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid page"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid page"})
 		return
 	}
 
 	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid limit"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid limit"})
 		return
 	}
 
 	var totalCount int64
 	if err := uc.db.Model(&models.User{}).Count(&totalCount).Error; err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var users []models.User
+	offset := (page - 1) * limit
+	if err := uc.db.Model(&models.User{}).Offset(offset).Limit(limit).Find(&users).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	var userResponses []models.UserResponse
-	offset := (page - 1) * limit
-	if err := uc.db.Model(&models.User{}).Offset(offset).Limit(limit).Select("id, email, first_name, last_name").Find(&userResponses).Error; err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+	for _, user := range users {
+		userResponses = append(userResponses, user.ToResponse())
 	}
 
 	c.JSON(http.StatusOK, gin.H{
@@ -50,7 +55,7 @@ func (uc *UserController) GetAllUsers(c *gin.Context) {
 		"meta": gin.H{
 			"page":       page,
 			"limit":      limit,
-			"totalPages": int(math.Ceil(float64(totalCount) / float64(limit))),
+			"totalPages": utils.CalculateTotalPages(totalCount, int64(limit)),
 			"totalCount": totalCount,
 		},
 	})
@@ -58,84 +63,69 @@ func (uc *UserController) GetAllUsers(c *gin.Context) {
 
 func (uc *UserController) GetUser(c *gin.Context) {
 	id := c.Param("id")
-	var user models.UserResponse
-	err := uc.db.Model(&models.User{}).First(&user, id).Error
-	if err != nil {
-		c.AbortWithStatusJSON(404, gin.H{"error": "User not found"})
+	var user models.User
+	if err := uc.db.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
-	c.JSON(200, user)
+	c.JSON(http.StatusOK, user.ToResponse())
 }
 
 func (uc *UserController) CreateUser(c *gin.Context) {
 	var user models.User
-	if err := c.BindJSON(&user); err != nil {
-		c.AbortWithStatusJSON(400, gin.H{"error": "Invalid request body"})
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
 		return
 	}
-	// Hash the user's password using bcrypt.
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"msg": "Error hashing password"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
 		return
 	}
 
-	// Replace the user's plaintext password with the hashed version.
 	user.Password = string(hashedPassword)
 
-	if err := uc.db.Create(&user).Error; err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
 		return
 	}
-	c.JSON(201, models.UserResponse{
-		ID:        user.ID,
-		Email:     user.Email,
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
-	})
+	c.JSON(http.StatusCreated, user.ToResponse())
 }
 
 func (uc *UserController) UpdateUser(c *gin.Context) {
 	id := c.Param("id")
 	var user models.User
-	err := uc.db.First(&user, id).Error
-	if err != nil {
-		c.AbortWithStatusJSON(404, gin.H{"error": "User not found"})
+	if err := uc.db.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
 
-	if err := c.BindJSON(&user); err != nil {
-		c.AbortWithStatusJSON(400, gin.H{"error": "Invalid request body"})
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
 		return
 	}
 
 	if err := uc.db.Save(&user).Error; err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(200, models.UserResponse{
-		ID:        user.ID,
-		Email:     user.Email,
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
-	})
+	c.JSON(http.StatusOK, user.ToResponse())
 }
 
 func (uc *UserController) DeleteUser(c *gin.Context) {
-
 	id := c.Param("id")
 	var user models.User
-	err := uc.db.First(&user, id).Error
-	if err != nil {
-		c.AbortWithStatusJSON(404, gin.H{"error": "User not found"})
+	if err := uc.db.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
 
 	if err := uc.db.Delete(&user).Error; err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.Status(204)
+	c.Status(http.StatusNoContent)
 }
