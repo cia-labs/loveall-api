aaf42fbc0f71446ed9fe8262d6b41b2938318e92
Vamsi Anamalamudi <vamsianamalamudi@Vamsis-MacBook-Pro.local>
2023-09-04 02:04:01 +0530

    typo

--- internal/zaplog/zaplog.go
+++ internal/zaplog/zaplog.go
@@ -17,8 +17,8 @@ func init() {
 	zapconfig := zap.NewProductionEncoderConfig()
 	zapconfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	fileEncoder := zapcore.NewJSONEncoder(zapconfig)
-	// logFile, _ := os.OpenFile(fmt.Sprintf("/var/log/loveall/loveall.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	logFile, _ := os.OpenFile(fmt.Sprintf("/Users/vamsianamalamudi/github/love-all-backend/loveall.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile, _ := os.OpenFile(fmt.Sprintf("/var/log/loveall/loveall.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// logFile, _ := os.OpenFile(fmt.Sprintf("/Users/vamsianamalamudi/github/love-all-backend/loveall.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	writer := zapcore.AddSync(logFile)
 	defaultLogLevel := zapcore.DebugLevel
 	core := zapcore.NewTee(zapcore.NewCore(fileEncoder, writer, defaultLogLevel))
